dce01a5a2d9bf891fe8b70b783e16f31e2974bf7
Michael Reichert <michael.reichert@geofabrik.de>
2016-08-22 14:22:25 +0200

    first reorder the table, then create indexes

--- src/table.cpp
+++ src/table.cpp
@@ -95,15 +95,15 @@ Table::~Table() {
         }
         std::cerr << "committing table " << m_name << " …";
         commit();
-        if (m_config.m_id_index && !m_config.m_append) {
-            create_id_index();
-        }
         if ((m_columns.get_type() != TableType::UNTAGGED_POINT && !m_config.m_append)
                 || (m_columns.get_type() == TableType::UNTAGGED_POINT && m_config.m_all_geom_indexes && !m_config.m_append)) {
             if (m_config.m_order_by_geohash) {
                 order_by_geohash();
             }
             create_geom_index();
+        }
+        if (m_config.m_id_index && !m_config.m_append) {
+            create_id_index();
         }
         PQfinish(m_database_connection);
     }
