0a5faa1d225763737dd8368c59ec1e385bb0fabe
Michael Reichert <michael.reichert@geofabrik.de>
2016-12-23 10:40:46 +0100

    convert memory to MB and time to hours

--- util/memory-usage-calculator.py
+++ util/memory-usage-calculator.py
@@ -1,23 +1,32 @@
 #! /usr/bin/env python3
 
+# Usage: ./memory-usage-calculator.py LOGFILE [INTERVAL]
+#
+# Example: ./memory-usage-calculator.py memory-usage.log 5
+#   to write a CSV file with the memory usage of every fifth second
+
 import sys
 
 pgimporter_running = False
 second = 0
+second_interval = 1
+if len(sys.argv) >= 3:
+    second_interval = int(sys.argv[2])
 current_memory_usage = 0
 logfile = open(sys.argv[1], "r")
 csvfile = sys.stdout 
 for line in logfile:
     if pgimporter_running == False:
-        if line.find("=== start ===") >= 0:
+        if line.find("====start====") >= 0:
             pgimporter_running = True
     elif line.find("grep") >= 0:
         continue
     elif line.startswith("====="):
-        csvfile.write(str(second) + "\t" + str(current_memory_usage) + "\n")
+        if second % second_interval == 0:
+            csvfile.write(str(second/3600) + "\t" + str(current_memory_usage/1024) + "\n")
         current_memory_usage = 0
         second += 1
-    elif line.find("=== stop ===") >= 0:
+    elif line.find("====stop====") >= 0:
         pgimporter_running = False
         logfile.close()
         csvfile.close()
@@ -31,4 +40,3 @@ for line in logfile:
             continue
 logfile.close()
 csvfile.close()
-        
